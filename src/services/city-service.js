17dc8585840c7376fd006ff1bf4b26ff64946372
kamal0620040 <neupane0620040@outlook.com>
2023-07-30 18:28:08 +0545

    Added Service layer of City

---
+++ src/services/city-service.js
@@ -0,0 +1,53 @@
+const { CityRepository } = require('../repositories/index')
+
+class CityService {
+    constructor(){
+        this.cityRepository = new CityRepository();
+    }
+
+    /* Here the service layer is just calling
+    ** the repository layer
+    ** as the city model is quite simple
+    */
+    async createCity(data){
+        try {
+            const city = await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at the service layer");
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId){
+        try {
+            const response = await this.cityRepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong at the service layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await this.cityRepository.updateCity(cityId,data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at the service layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at the service layer");
+            throw {error};
+        }
+    }
+}
+
+module.exports = CityService;
