ac1f302d8aa94fabda117747919e030551a80904
kamal0620040 <neupane0620040@outlook.com>
2023-07-30 21:25:39 +0545

    Added routers to complete create city api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const CityController = require('../../controllers/city-controller');
 const {InfoController} = require('../../controllers');
 
 const router = express.Router();
 
-router.get('/info',InfoController.info)
+router.get('/info',InfoController.info);
+router.post('/city',CityController.create);
 
 module.exports = router;
