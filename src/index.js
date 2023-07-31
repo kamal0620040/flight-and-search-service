7dd1898c30d00fd0df839f1608979e966e0c6948
kamal0620040 <neupane0620040@outlook.com>
2023-07-31 20:35:43 +0545

    Added model, migration and seed for Airplane

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 const bodyParser = require('body-parser');
+const db = require('./models/index');
+// const {City, Airport} = require('./models/index');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +11,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes);
 
-app.listen(ServerConfig.PORT, ()=>{
+app.listen(ServerConfig.PORT, async ()=>{
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+    
+    if(process.env.SYNC_DB){
+        db.sequelize.sync({alter:true});
+    }
+    
+    // const airports = await city.getAirports();
+    // console.log(airports);
 })
