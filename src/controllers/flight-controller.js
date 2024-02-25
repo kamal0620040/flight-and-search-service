bf43a64dd582cc7314e2002d7263f0a9a3f5901b
kamal0620040 <neupane0620040@outlook.com>
2024-02-25 13:16:23 +0545

    Build a API for getting a particualar flight

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,63 +1,84 @@
-const {FlightService} = require('../services/index');
-const { StatusCodes } = require('http-status-codes');
+const { FlightService } = require("../services/index");
+const { StatusCodes } = require("http-status-codes");
 const flightService = new FlightService();
 
 /*
-* POST
-* data -> req.body
-*/
+ * POST
+ * data -> req.body
+ */
 const create = async (req, res) => {
-    try{
-        const flightRequestData = {
-            flightNumber: req.flight.flightNumber,
-            airplaneId: req.body.airplaneId,
-            departureAirportId: req.body.departureAirportId,
-            arrivalAirportId: req.body.arrivalAirportId,
-            arrivalTime: req.body.arrivalTime,
-            departureTime: req.body.departureTime,
-            price: req.body.price,
-        }
+  try {
+    const flightRequestData = {
+      flightNumber: req.flight.flightNumber,
+      airplaneId: req.body.airplaneId,
+      departureAirportId: req.body.departureAirportId,
+      arrivalAirportId: req.body.arrivalAirportId,
+      arrivalTime: req.body.arrivalTime,
+      departureTime: req.body.departureTime,
+      price: req.body.price,
+    };
 
-        const flight = await flightService.createFlight(flightRequestData);
+    const flight = await flightService.createFlight(flightRequestData);
 
-        return res.status(StatusCodes.CREATED).json({
-            data: flight,
-            success: true,
-            message: 'Successfully created a flight',
-            err: {}
-        });
-    } catch(error){
-        console.log(error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            data: {},
-            success: false,
-            message: 'Not able to create a flight',
-            err: error,
-        });
-    }
-}
+    return res.status(StatusCodes.CREATED).json({
+      data: flight,
+      success: true,
+      message: "Successfully created a flight",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      data: {},
+      success: false,
+      message: "Not able to create a flight",
+      err: error,
+    });
+  }
+};
 
-const getAll = async(req, res) => {
-    try {
-        const response = await flightService.getAllFlightData(req.query);
-        return res.status(StatusCodes.OK).json({
-            data: response,
-            success: true,
-            err: {},
-            message: 'Successfully fetched the flights',
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            data: {},
-            success: false,
-            message: 'Not able to fetch the flight',
-            err: error,
-        });
-    }
-}
+const getAll = async (req, res) => {
+  try {
+    const response = await flightService.getAllFlightData(req.query);
+    return res.status(StatusCodes.OK).json({
+      data: response,
+      success: true,
+      err: {},
+      message: "Successfully fetched the flights",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch the flight",
+      err: error,
+    });
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const response = await flightService.getFlight(req.params.id);
+    return res.status(StatusCodes.OK).json({
+      data: response,
+      success: true,
+      err: {},
+      message: "Successfully fetched the flight",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch the flight",
+      err: error,
+    });
+  }
+};
 
 module.exports = {
-    create,
-    getAll,
-}
+  create,
+  getAll,
+  get,
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const CityController = require('../../controllers/city-controller');
-const {InfoController} = require('../../controllers');
-const FlightController = require('../../controllers/flight-controller');
-const AirportController = require('../../controllers/airport-controller');
+const express = require("express");
+const CityController = require("../../controllers/city-controller");
+const { InfoController } = require("../../controllers");
+const FlightController = require("../../controllers/flight-controller");
+const AirportController = require("../../controllers/airport-controller");
 
-const {FlightMiddlewares} = require('../../middlewares/index');
+const { FlightMiddlewares } = require("../../middlewares/index");
 
 const router = express.Router();
 
-router.get('/info',InfoController.info);
-router.post('/city',CityController.create);
-router.delete('/city/:id',CityController.destroy);
-router.get('/city/:id',CityController.get);
-router.get('/city', CityController.getAll);
-router.patch('/city/:id',CityController.update);
+router.get("/info", InfoController.info);
+router.post("/city", CityController.create);
+router.delete("/city/:id", CityController.destroy);
+router.get("/city/:id", CityController.get);
+router.get("/city", CityController.getAll);
+router.patch("/city/:id", CityController.update);
 
-router.post('/flights', FlightMiddlewares.validateCreateFlight, FlightController.create);
-router.get('/flights',FlightController.getAll);
+router.post(
+  "/flights",
+  FlightMiddlewares.validateCreateFlight,
+  FlightController.create
+);
+router.get("/flights", FlightController.getAll);
 
-router.post('/airports', AirportController.create);
+router.post("/airports", AirportController.create);
+
+router.get("/flights/:id", FlightController.get);
 
 module.exports = router;
